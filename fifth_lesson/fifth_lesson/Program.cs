5134a65e9ffee86d5f52b9b0552d1ab3fa622c2a
morafeus <aadamovich2003@gmail.com>
2025-09-29 11:03:08 +0300

    start branch

---
+++ fifth_lesson/fifth_lesson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifth_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
