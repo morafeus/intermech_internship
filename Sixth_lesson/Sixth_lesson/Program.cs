36859e4e38f3ec2ad2a2d3d4dcbf73c4297f077d
morafeus <aadamovich2003@gmail.com>
2025-09-30 14:30:55 +0300

    change assembly connection way

--- Sixth_lesson/Sixth_lesson/Program.cs
+++ Sixth_lesson/Sixth_lesson/Program.cs
@@ -10,15 +10,16 @@ namespace Sixth_lesson
     {
         static void Main(string[] args)
         {
-            var relativePath = @"..\..\..\TemperatureConverter\bin\Debug\TemperatureConverter.dll"; // Корректируйте путь при необходимости
-            var assembly = Assembly.LoadFrom(relativePath);
+            var relativePath = @"..\..\..\TemperatureConverter\bin\Debug\TemperatureConverter.dll";
+            AssemblyName name = AssemblyName.GetAssemblyName(relativePath);
+            var assembly = Assembly.Load(name);
 
             var type = assembly.GetType("TemperatureConverter.Converter");
             var instance = Activator.CreateInstance(type);
 
             try
             {
-                var tempResult = type.GetMethod("CelsiusToFahrenheit").Invoke(instance, new object[] { -3225.5 });
+                var tempResult = type.GetMethod("CelsiusToFahrenheit").Invoke(instance, new object[] { -25.5 });
                 Console.WriteLine(tempResult);
             }
             catch (TargetInvocationException ex) { Console.WriteLine(ex.InnerException.Message); }
