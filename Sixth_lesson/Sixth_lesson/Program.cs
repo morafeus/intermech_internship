bf424784978265d72a326ee83b270b10115445a8
morafeus <aadamovich2003@gmail.com>
2025-09-30 12:07:49 +0300

    lesson 6 done

---
+++ Sixth_lesson/Sixth_lesson/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using System;
+using System.Reflection;
+
+namespace Sixth_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var relativePath = @"..\..\..\TemperatureConverter\bin\Debug\TemperatureConverter.dll"; // Корректируйте путь при необходимости
+            var assembly = Assembly.LoadFrom(relativePath);
+
+            var type = assembly.GetType("TemperatureConverter.Converter");
+            var instance = Activator.CreateInstance(type);
+
+            var tempResult = type.GetMethod("CelsiusToFahrenheit").Invoke(instance, new object[] { 25.5});
+            Console.WriteLine(tempResult);
+
+        }
+    }
+}
