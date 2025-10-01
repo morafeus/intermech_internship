c1e5cbaacc10da65884fecf3e3892c0e45418100
morafeus <aadamovich2003@gmail.com>
2025-10-01 17:36:52 +0300

    start lesson 9

---
+++ 9_lesson/9_lesson/Monitoring.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _9_lesson
+{
+    public class Monitoring
+    {
+        private static long MBSCALE = 1024 * 1024;
+
+        private double _memoryLimit;
+        private double _memoryWarning;
+
+        public long GetMemoryValue()
+        {
+            Process process = Process.GetCurrentProcess();
+            long memoryUsedInBytes = process.WorkingSet64;
+            return memoryUsedInBytes * MBSCALE;
+        }
+
+        public void SetLimits(int memoryLimit)
+        {
+            _memoryLimit = memoryLimit;
+            _memoryWarning = memoryLimit * 0.9;
+        }
+
+        public void CheckMemory()
+        {
+            var memory = GetMemoryValue();
+
+            Console.WriteLine($"расходуется {memory} Mb памяти из допустимых {_memoryWarning} Mb");
+
+            if (memory < _memoryWarning)
+            {
+                Console.WriteLine($"использование памяти в норме");
+                return;
+            }
+            else if( memory > _memoryLimit  && memory < _memoryLimit)
+            {
+                Console.WriteLine($"приложение потребляет слишком много памяти");
+            }
+            else
+            {
+                Console.WriteLine($"приложение превышает допустимый режим потребления памяти");  
+            }
+        }
+
+
+
+
+        
+    }
+}
---
+++ 9_lesson/9_lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
