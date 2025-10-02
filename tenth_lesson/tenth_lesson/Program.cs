175476938891c5ab9f9496e33e5b9a6628baa91f
morafeus <aadamovich2003@gmail.com>
2025-10-02 15:50:45 +0300

    lesson done

---
+++ tenth_lesson/tenth_lesson/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using tenth_lesson.TemplateMethod;
+
+namespace tenth_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainReport = new MainReport();
+            mainReport.GenerateReport();
+
+            var secondaryReport = new SecondaryReport();
+            secondaryReport.GenerateReport();
+        }
+    }
+}
