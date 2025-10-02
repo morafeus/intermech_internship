96367bf4cf337572ecf1595c5be32419a8585b5a
morafeus <aadamovich2003@gmail.com>
2025-10-02 17:27:08 +0300

    start lesson 11

---
+++ eleventh_lesson/eleventh_lesson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace eleventh_lesson
+{
+    internal class Program
+    {
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            var threadFirst = new Thread(()=> 
+            {
+                var info = File.ReadAllText(@"D:\internship\file_system\first.txt");
+                Console.WriteLine(info);
+            });
+
+            var threadSecond = new Thread(()=> 
+            {
+                var info = File.ReadAllText(@"D:\internship\file_system\second.txt");
+                Console.WriteLine(info);
+            });
+
+            threadFirst.Start();
+            threadSecond.Start();
+        }
+
+        static void SaveThird(string data)
+        {
+            lock (locker)
+            {
+               
+            }
+        }
+    }
+}
