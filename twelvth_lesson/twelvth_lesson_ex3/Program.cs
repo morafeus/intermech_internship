8dd31983f0496d6ac700da4779f5ba292308a917
morafeus <aadamovich2003@gmail.com>
2025-10-03 11:45:43 +0300

    fix constant guid

--- twelvth_lesson/twelvth_lesson_ex3/Program.cs
+++ twelvth_lesson/twelvth_lesson_ex3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace twelvth_lesson_ex3
@@ -6,15 +8,21 @@ namespace twelvth_lesson_ex3
     internal class Program
     {
 
-        const string MUTEX_ID = "Global\\{B1E7934A-F688-417f-8FCB-65C3985E9E27}"; 
+        static string appGuid = 
+                ((GuidAttribute) Assembly
+            .GetExecutingAssembly()
+            .GetCustomAttributes(typeof(GuidAttribute), false)
+            .GetValue(0)
+        ).Value.ToString(); 
 
         static void Main(string[] args)
-        { 
-            var mutex = new Mutex(false, MUTEX_ID);
-            
+        {
+            string mutexId = string.Format("Global\\{{{0}}}", appGuid);
+            var mutex = new Mutex(false, mutexId);
+
             if (!mutex.WaitOne(TimeSpan.FromSeconds(5), false))
             {
-                Console.WriteLine("Another instance of this program is running");
+                Console.WriteLine("приложение уже запущено");
                 Environment.Exit(0);
             }
                     
