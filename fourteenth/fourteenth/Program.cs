5a8a3d0acf47b96105fa81e50e2f16e10a8a9c1d
morafeus <aadamovich2003@gmail.com>
2025-10-03 15:47:17 +0300

    lesson is done

---
+++ fourteenth/fourteenth/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace fourteenth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => {
+                Parallel.Invoke(FirstMethod, SecondMethod);
+            });
+            for(int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+        }
+
+        static void FirstMethod()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("первый метод");
+        }
+
+        static void SecondMethod()
+        {
+            Thread.Sleep(4000);
+            Console.WriteLine("второй метод");
+        }
+    }
+}
