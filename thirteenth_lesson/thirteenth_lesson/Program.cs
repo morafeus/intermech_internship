aec86e678fd56a00236c88c0c3e31fca36f8b3dc
morafeus <aadamovich2003@gmail.com>
2025-10-03 13:24:59 +0300

    lesson 13 done

---
+++ thirteenth_lesson/thirteenth_lesson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace thirteenth_lesson
+{
+    internal class Program
+    {
+        public delegate string MyDelegate(string name);
+
+        static void Main(string[] args)
+        {
+            var handler = new MyDelegate(DoSomething);
+
+            var result = handler.BeginInvoke("Alexey", AsyncDoSomething, handler);
+        }
+
+        static string DoSomething(string name)
+        {
+            if (name != null)
+            {
+
+                return "hello, " + name;
+            }
+            else
+                return null;
+        }
+
+        static void AsyncDoSomething(IAsyncResult result)
+        {
+            var handler = (MyDelegate)result.AsyncState;
+            var finalResult = handler.EndInvoke(result);
+            if(finalResult != null)
+            {
+                Console.WriteLine(finalResult + ", and good luck to you");
+            }
+        }
+    }
+}
