6f19ac8bc5fe2546710bbe8c4ef6b9a108e9a4da
morafeus <aadamovich2003@gmail.com>
2025-10-03 17:14:21 +0300

    create main window

---
+++ fifteenth_lesson/fifteenth_lesson/DatabaseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace fifteenth_lesson
+{
+    public class DatabaseManager
+    {
+        public static async Task<string> GetData()
+        {
+            Thread.Sleep(4000);
+            return "подключен к базе данных"; 
+        }
+    }
+}
---
+++ fifteenth_lesson/fifteenth_lesson/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace fifteenth_lesson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxStatus.Text += DatabaseManager.GetData().Result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
