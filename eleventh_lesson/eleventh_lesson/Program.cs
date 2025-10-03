e30366aebd26c4f9ca23dae59aeaee74660344bb
morafeus <aadamovich2003@gmail.com>
2025-10-03 09:49:53 +0300

    lesson done

--- eleventh_lesson/eleventh_lesson/Program.cs
+++ eleventh_lesson/eleventh_lesson/Program.cs
@@ -6,31 +6,42 @@ namespace eleventh_lesson
 {
     internal class Program
     {
+        private const string FIRSTPATH = @"D:\internship\file_system\first.txt";
+        private const string SECONDPATH = @"D:\internship\file_system\second.txt";
+        private const string THIRDPATH = @"D:\internship\file_system\third.txt";
+
         static object locker = new object();
 
         static void Main(string[] args)
         {
             var threadFirst = new Thread(()=> 
             {
-                var info = File.ReadAllText(@"D:\internship\file_system\first.txt");
-                Console.WriteLine(info);
+                var info = File.ReadAllText(FIRSTPATH);
+                SaveThird(info);
             });
 
             var threadSecond = new Thread(()=> 
             {
-                var info = File.ReadAllText(@"D:\internship\file_system\second.txt");
-                Console.WriteLine(info);
+                var info = File.ReadAllText(SECONDPATH);
+                SaveThird(info);
             });
 
             threadFirst.Start();
             threadSecond.Start();
+
+            threadFirst.Join();
+            threadSecond.Join();
         }
 
         static void SaveThird(string data)
         {
             lock (locker)
             {
-               
+                using (StreamWriter writer = new StreamWriter(THIRDPATH, true))
+                {
+                    Thread.Sleep(1000);
+                    writer.WriteLine(data);
+                }
             }
         }
     }
