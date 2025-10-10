e0deebfafc083e7d8ff4a2de3a7063d1c2259c6b
morafeus <aadamovich2003@gmail.com>
2025-10-10 12:23:41 +0300

    start project

---
+++ WebAPI_internship/WebAPI_internship/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_internship.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hello world";
+        }
+    }
+}
